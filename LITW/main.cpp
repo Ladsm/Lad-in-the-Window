c94852e74e919ea032896b02244ec367938c90a3
Ladsm <BarryKotler0@gmail.com>
2026-03-13 03:59:25 +0200

    added Readme

--- LITW/main.cpp
+++ LITW/main.cpp
@@ -9,33 +9,11 @@
 
 class MenuWindow : public Window {
 public:
-    int selection = 0;
-    MenuWindow() : Window("Main Menu", 30, 10) {}
-
-    void Draw(std::ostream& buffer) override {
-        Window::Draw(buffer);
-        buffer << "\033[38;2;0;0;0;48;2;192;192;192m";
-        std::string items[] = { "Start Game", "Options", "Exit" };
-        for (int i = 0; i < 3; i++) {
-            buffer << "\033[" << (y + 5 + i) << ";" << (x + 3) << "H";
-            buffer << (selection == i ? "-> " : "   ") << items[i];
-        }
-        buffer << "\033[0m";
-    }
-    void HandleInput(InputType input) {
-        if (input == InputType::MoveUp) selection = (selection > 0) ? selection - 1 : 2;
-        if (input == InputType::MoveDown) selection = (selection < 2) ? selection + 1 : 0;
-        if (input == InputType::Enter) {
-            switch (selection) {
-            case 0:
-                break;
-            case 1:
-                break;
-            case 2:
-                std::exit(0);
-                break;
+    MenuWindow(std::string title) : Window(title, 40, 10) {
+        AddWidget(new Button(2, 2, "Exit", [this]() {
+            std::exit(0);
             }
-        }
+        ));
     }
 };
 class TextWindow : public Window {
@@ -44,13 +22,27 @@ public:
         AddWidget(new Label(2, 2, msg));
         AddWidget(new Button(2, 5, "Close", [this]() {
             visible = false;
-            }));
+            }
+        ));
     }
     TextWindow(std::string title, std::string msg, int w, int h) : Window(title, w, h) {
         AddWidget(new Label(2, 2, msg));
         AddWidget(new Button(2, 5, "Close", [this]() {
             visible = false;
-            }));
+            }
+        ));
+    }
+};
+class README : public Window {
+public:
+    README() : Window("README", 40, 10) {
+        AddWidget(new Label(2, 2, "Lad in the Window, Ladsm 2026"));
+        AddWidget(new Label(2, 3, "-----------------------------"));
+        AddWidget(new Label(2, 4, "uhhhhh... enjoy the GUI in a TUI."));
+        AddWidget(new Button(2, 5, "Close", [this]() {
+            visible = false;
+            }
+        ));
     }
 };
 
@@ -59,17 +51,15 @@ int main() {
     WindowManager wm;
     StartMenuWindow start(&wm);
     start.AddItem("Main Menu", []() {
-        return new MenuWindow();
+        return new MenuWindow("main menu");
         });
     start.AddItem("System Status", []() {
         return new TextWindow("System status", "System is running smoothly!");
         });
-    start.AddItem("Malware", []() {
-        return new TextWindow("wana decript", "Oh no! your files have been encripted! pay me", 60, 10);
+    start.AddItem("README", []() {
+        return new README();
         });
     wm.SetStartMenu(&start);
     wm.AddWindow(&start);
-    TextWindow malware = TextWindow("wana decript", "Oh no! your files have been encripted! pay me", 60, 10);
-    wm.AddWindow(&malware);
     wm.Run();
 }
