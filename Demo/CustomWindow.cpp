b8b2be9299aa1c91b5e18d4e36931850ae8d4cab
Ladsm <BarryKotler0@gmail.com>
2026-04-08 03:04:09 +0300

    added newer api to demos.

--- Demo/CustomWindow.cpp
+++ Demo/CustomWindow.cpp
@@ -5,10 +5,9 @@ class TextWindow : public Window {
 public:
     TextWindow(std::string title, std::string msg)
         : Window(title, 40, 10) {
-        AddWidget(std::make_unique <Label>(2, 2, msg));
-        AddWidget(std::make_unique <Button>(2, 5, "Close", [this]() {
-            wm.RemoveWindow(this);
-        }));
+        auto& vbox = Add<VertCon>(2, 2, 2);
+        vbox.Add<Label>(0, 0, msg);
+        vbox.Add<Button>(0, 0, "Close", [this]() { wm.RemoveWindow(this); });
     }
 };
 int main() {
--- Demo/helloworld.cpp
+++ Demo/helloworld.cpp
@@ -1,4 +1,3 @@
-
 #include <LITW.hpp>
 #include <memory>
 
@@ -7,13 +6,13 @@ int main() {
     //The window manager, stores windows.
     auto win = std::make_shared<Window>("Hello World", 30, 10);
     //creates a window, title "Hello World", with width 30 and height 10.
-    win->AddWidget(std::make_unique<Label>(2, 2, "Hello World"));
+    win->Add<Label>(2, 2, "Hello World");
     //adds a label widget at width 2 and height 2.
     wm.AddWindow(win); //adds window to window vector.
     wm.Run(); // runs the window manager loop(main loop).
     wm.exit(1);
-    /* no natral exit, returns 1 due there being no exit state.
-       use wm.exit because it reverts console state from the LITW state to the old state. */
+    /*  exit 1 because it would not normaly exit. 
+        It would only exits with F12, that just exits, not break the run loop. */
     return 0;
 }
 
