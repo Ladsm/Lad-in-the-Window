146d3c80285b191582544abbb3efa73d796fceb2
Ladsm <BarryKotler0@gmail.com>
2026-08-27 21:33:15 +0300

    Made alert able to handle longer strings.

--- include/windows/AlertWindow.hpp
+++ include/windows/AlertWindow.hpp
@@ -2,14 +2,19 @@
 #include "Window.hpp"
 #include "widgets/text/Label.hpp" 
 #include "widgets/Button.hpp"
+#include "widgets/containers/VerticalContainer.hpp"
 
 class AlertWindow : public Window {
 public:
-    AlertWindow(std::string msg, WindowManager* wm) : Window("Alert", 40, 8) {
-        AddWidget(std::make_unique<CenteredLabel>(2, msg));
-        AddWidget(std::make_unique<Button>(15, 5, "  OK  ", [wm, this]() {
+    AlertWindow(std::string msg, WindowManager* wm) : Window("Alert", 40, 9) {
+        if (msg.size() > 35) {
+            startWidth = msg.size() + 4;
+            width = startWidth;
+        }
+        auto& vbox = Add<VerticalContainer>(2, 2, 1);
+        vbox.Add<Label>(msg);
+        vbox.Add<Button>("  OK  ", [wm, this]() {
             wm->RemoveWindow(this);
-            }));
-        focusedWidget = 1;
+        });
     }
 };
